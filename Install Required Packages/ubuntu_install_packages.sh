466624e700752f51efa9e9d91bcb58c32ee6b7a0
mhykari <mhykari@gmail.com>
2024-09-23 12:58:54 +0330

    add install-packages-files

---
+++ Install Required Packages/ubuntu_install_packages.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Update package list
+echo "Updating package list..."
+sudo apt update
+
+# Install required packages
+echo "Installing required packages..."
+sudo apt install -y \
+    curl \
+    wget \
+    vim \
+    git \
+    zip \
+    rsync \
+    htop \
+    python3 \
+    less \
+    lsof \
+    iotop \
+    dstat \
+    sysstat \
+    tree \
+    net-tools \
+    dnsutils \
+    tcpdump \
+    traceroute \
+
+
+# Add any additional packages here
+# sudo apt install -y <package_name>
+
+# Clean up
+echo "Cleaning up..."
+sudo apt autoremove -y
+
+echo "All packages installed successfully!"
